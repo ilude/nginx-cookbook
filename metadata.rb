bfab310b57e8ecbe6278fb13019827aee6a6a405
Mike Glenn <mglenn@ilude.com>
2014-08-23 01:47:33 -0400

    detect vagrant

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'Mike Glenn'
 license          'All rights reserved'
 description      'Installs/Configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
